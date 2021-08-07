a987101676fc0f05ed4a6e25dad03e4f61aa757f
jcgsr <cabralguerra@gmail.com>
2021-08-07 15:20:38 -0300

    navbar init

---
+++ src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+
+function Navbar() {
+  return (
+    <nav>
+      <div className="nav-logo">
+        <p>English?! Easy</p>
+      </div>
+      <div className="nav-content">
+        <span>6</span>
+        <span>7</span>
+        <span>8</span>
+        <span>9</span>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
