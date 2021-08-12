da1ee3cc98922dfc3636caa897ed2e6552ce52eb
jcgsr <cabralguerra@gmail.com>
2021-08-12 17:53:56 -0300

    oitavo continued

--- src/constants/oitavoDados.js
+++ src/constants/oitavoDados.js
@@ -3,13 +3,33 @@ import React from "react";
 const oitavoDados = [
   {
     id: 1,
-    url: "/anos/oitavo/adjectives",
+    url: "/anos/oitavo/comparative",
     text: "Adjectives (Comparative)",
   },
   {
     id: 2,
     url: "/anos/oitavo/relative-pronouns",
     text: "Relative Pronouns",
+  },
+  {
+    id: 3,
+    url: "/anos/oitavo/superlative",
+    text: "Adjectives (Superlative)",
+  },
+  {
+    id: 4,
+    url: "/anos/oitavo/begoingto",
+    text: "Future (Be Going To)",
+  },
+  {
+    id: 5,
+    url: "/anos/oitavo/will",
+    text: "Future (Will)",
+  },
+  {
+    id: 6,
+    url: "/anos/oitavo/prefixsufix",
+    text: "Prefixes and Sufixes",
   },
 ];
 
